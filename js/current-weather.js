886720270771f14665736ebe5b041b41b52c8291
shih <Poetic0124@icloud.com>
2026-01-29 18:32:30 +0800

    加入定位

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,6 +1,122 @@
 // const apiKey = CWB_API_KEY; TODO:設定成全域api key
 
 const currentWeatherModel = {
+  // 取得GPS定位
+  async getLocalion() {
+    if (!navigator.geolocation) {
+      console.log(
+        'Browser does not support location services; Uses IP address instead.'
+      );
+      return { GPS: null };
+    }
+    try {
+      // 獲取經緯度
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, {
+          timeout: 5000,
+          enableHighAccuracy: true,
+          maximumAge: 0,
+        });
+      });
+      // console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      return { GPS: { latitude: latitude, longitude: longitude } };
+    } catch (error) {
+      console.log('Unable to get location data', error);
+      return { GPS: null };
+    }
+  },
+  // 取得所在城市資料
+  async getCityName(lat, lon) {
+    // 如果有經緯度就帶入
+    let url = `https://api.bigdatacloud.net/data/reverse-geocode-client?localityLanguage=zh`;
+
+    if (lat && lon) {
+      url += `&latitude=${lat}&longitude=${lon}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error('Unable to get location city data');
+
+      const data = await response.json();
+
+      // 優先順序：城市名 > 行政區名 > 預設城市
+      const cityName = data.city || data.principalSubdivision || '臺北市';
+
+      console.log(
+        `定位來源: ${data.lookupSource === 'coordinates' ? 'GPS' : 'IP 地址'}`
+      );
+      console.log('偵測到的城市：', cityName);
+
+      return cityName;
+    } catch (error) {
+      console.error('定位轉換失敗，回傳預設城市:', error);
+      return '臺北市';
+    }
+  },
+  // 將城市名稱轉為氣象資料名稱
+  formatTaiwanCity(name) {
+    if (!name|| typeof name !== 'string') return '臺北市';
+
+    // 城市對照表
+    const cityMap = {
+      臺北: '臺北市',
+      台北: '臺北市',
+      Taipei: '臺北市',
+      新北: '新北市',
+      'New Taipei': '新北市',
+      桃園: '桃園市',
+      Taoyuan: '桃園市',
+      臺中: '臺中市',
+      台中: '臺中市',
+      Taichung: '臺中市',
+      臺南: '臺南市',
+      台南: '臺南市',
+      Tainan: '臺南市',
+      高雄: '高雄市',
+      Kaohsiung: '高雄市',
+      基隆: '基隆市',
+      Keelung: '基隆市',
+      新竹市: '新竹市',
+      'Hsinchu City': '新竹市',
+      新竹縣: '新竹縣',
+      'Hsinchu County': '新竹縣',
+      苗栗: '苗栗縣',
+      Miaoli: '苗栗縣',
+      彰化: '彰化縣',
+      Changhua: '彰化縣',
+      南投: '南投縣',
+      Nantou: '南投縣',
+      雲林: '雲林縣',
+      Yunlin: '雲林縣',
+      嘉義市: '嘉義市',
+      'Chiayi City': '嘉義市',
+      嘉義縣: '嘉義縣',
+      'Chiayi County': '嘉義縣',
+      屏東: '屏東縣',
+      Pingtung: '屏東縣',
+      宜蘭: '宜蘭縣',
+      Yilan: '宜蘭縣',
+      花蓮: '花蓮縣',
+      Hualien: '花蓮縣',
+      臺東: '臺東縣',
+      台東: '臺東縣',
+      Taitung: '臺東縣',
+      澎湖: '澎湖縣',
+      Penghu: '澎湖縣',
+      金門: '金門縣',
+      Kinmen: '金門縣',
+      連江: '連江縣',
+      馬祖: '連江縣',
+      Lienchiang: '連江縣',
+    };
+
+    const foundKey = Object.keys(cityMap).find((key) => name.includes(key));
+
+    return foundKey ? cityMap[foundKey] : '臺北市';
+  },
   // 抓氣象資料
   async fetchWeatherInfo(URL) {
     try {
@@ -135,7 +251,6 @@ const currentWeatherView = {
       '.current-weather_info_container_ci_text_value'
     );
 
-    console.log();
 
     // 注入資料
     city.textContent = weatherData.cityNameValue;
@@ -165,13 +280,16 @@ const currentWeatherController = {
       'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-CDCFFC63-52A1-44F8-AB6B-73AE3E2CD128';
     const wholeWeatherInfo = await currentWeatherModel.fetchWeatherInfo(URL);
 
+    // 取得所在城市
+    city = await currentWeatherController.currentLocation();
+
     // 取得單一城市氣象資料
-    const currentCity = '臺北市';
+    const currentCity = city? city:'臺北市';
     const thisCityData = currentWeatherModel.chooseOneCityWeatherData(
       wholeWeatherInfo,
       currentCity
     );
-    console.log(thisCityData);
+    // console.log(thisCityData);
     // 取得要帶傘嗎訊息
     const umbrellaMsg = currentWeatherModel.umbrellaMsg(thisCityData.pop);
 
@@ -180,7 +298,7 @@ const currentWeatherController = {
       thisCityData,
       umbrellaMsg
     );
-    console.log(weatherData);
+    // console.log(weatherData);
 
     // 渲染畫面
     currentWeatherView.renderHeader(weatherData);
@@ -191,7 +309,21 @@ const currentWeatherController = {
     const time = now.time;
     currentWeatherView.renderTime(date, time);
   },
+  async currentLocation() {
+    // 先看能否取得gps取得定位
+    const location = await currentWeatherModel.getLocalion();
+    const lat = location.GPS?.latitude;
+    const lon = location.GPS?.longitude;
+
+    // 取得所在城市
+    if (lat && lon) {
+      const cityName = await currentWeatherModel.getCityName(lat, lon);
+      return currentWeatherModel.formatTaiwanCity(cityName);
+    } else {
+      const cityName = await currentWeatherModel.getCityName(null, null);
+      return currentWeatherModel.formatTaiwanCity(cityName);
+    }
+  },
 };
 
 // currentWeatherController.WeatherInfo();
-currentWeatherController.currentTime();
